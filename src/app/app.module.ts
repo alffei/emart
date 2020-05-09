2dd92b665de89462068b4f5f1d1293e244d8ecca
chenpengfei <alffei@163.com>
2020-05-09 21:40:37 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {UserModule} from './user/user.module';
+import {ProductModule} from './product/product.module';
+import {HomeModule} from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    SharedModule,
+    UserModule,
+    ProductModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
